2923c2b15da277519bb924c5accac8efacdfb49c
andi <andreas@orangeqs.com>
2022-10-23 14:23:15 +0200

    Added name == main to run_visualizer

--- run_visualizer.py
+++ run_visualizer.py
@@ -1,15 +1,17 @@
 from frame import frame 
 import cv2
 
-frame = frame()
 
-while frame.rval:
-    frame.refresh_input()
-    frame.normalize_and_blur()
-    frame.edge_detection()
-    frame.display_frame()
+if __name__ == "__main__":
+    frame = frame()
 
-    # enable key input to stop the application
-    key = cv2.waitKey(20) 
-    if key == 27: # exit on ESC
-        break
+    while frame.rval:
+        frame.refresh_input()
+        frame.normalize_and_blur()
+        frame.edge_detection()
+        frame.display_frame()
+
+        # enable key input to stop the application
+        key = cv2.waitKey(20) 
+        if key == 27: # exit on ESC
+            break
